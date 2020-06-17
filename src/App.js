2f851184c287a03d3210aedd53fc3740b6a89359
Prakhar <34737618+prakhargloify@users.noreply.github.com>
2020-06-17 20:02:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React from "react";
 import { parse } from "papaparse";
+import "./App.css";
 
 export default function App() {
   const [highlighted, setHighlighted] = React.useState(false);
-  const [contacts, setContacts] = React.useState([
-    { email: "fake@gmail.com", name: "Fake" },
-  ]);
+  const [contacts, setContacts] = React.useState([]);
+  const [filteredContacts, setFilteredContacts] = React.useState([]);
+  const [fileDelimiter, setFileDelimiter] = React.useState();
+  const [delimiter, setDelimiter] = React.useState();
+  const [lines, setLines] = React.useState();
+
+  function setLimitedArray(e) {
+    setLines(e.target.value);
+    let newArray = contacts.filter((i, index) => ++index <= e.target.value);
+    if (e.target.value) setFilteredContacts(newArray);
+    else setFilteredContacts(contacts);
+  }
+
+  function handleChange(event) {
+    let reader = new FileReader();
+    reader.onloadend = () => {
+      const text = reader.result;
+      for (var i = 0; i < text.length; i++) {
+        if (
+          (text.charCodeAt(i) >= 33 && text.charCodeAt(i) <= 47) ||
+          (text.charCodeAt(i) >= 123 && text.charCodeAt(i) <= 126)
+        ) {
+          setDelimiter(text.charAt(i));
+          setFileDelimiter(text.charAt(i));
+        }
+      }
+      const result = parse(text, { header: true });
+      setFilteredContacts((existing) => [...existing, ...result.data]);
+      setContacts((existing) => [...existing, ...result.data]);
+    };
+    reader.readAsText(event.target.files[0]);
+  }
 
   return (
-    <div>
+    <div className="main-body">
       <h1 className="text-center text-4xl">Contact Import</h1>
       <div
-        className={`p-6 my-2 mx-auto max-w-md border-2 ${
-          highlighted ? "border-green-600 bg-green-100" : "border-gray-600"
-        }`}
+        className={`${highlighted ? "border-green" : "border-gray"}`}
         onDragEnter={() => {
           setHighlighted(true);
         }}
@@ -26,26 +54,70 @@ export default function App() {
         onDrop={(e) => {
           e.preventDefault();
           setHighlighted(false);
-
-          Array.from(e.dataTransfer.files)
-            .filter((file) => file.type === "text/csv")
-            .forEach(async (file) => {
-              const text = await file.text();
-              const result = parse(text, { header: true });
-              setContacts((existing) => [...existing, ...result.data]);
-            });
+          Array.from(e.dataTransfer.files).forEach(async (file) => {
+            const text = await file.text();
+            for (var i = 0; i < text.length; i++) {
+              if (
+                (text.charCodeAt(i) >= 33 && text.charCodeAt(i) <= 47) ||
+                (text.charCodeAt(i) >= 123 && text.charCodeAt(i) <= 126)
+              ) {
+                setDelimiter(text.charAt(i));
+                setFileDelimiter(text.charAt(i));
+              }
+            }
+            const result = parse(text, { header: true });
+            setFilteredContacts((existing) => [...existing, ...result.data]);
+            setContacts((existing) => [...existing, ...result.data]);
+          });
         }}
       >
-        DROP HERE
+        <span>DROP HERE or </span>
+        <input type="file" className="upload-button" onChange={handleChange} />
       </div>
-
-      <ul>
-        {contacts.map((contact) => (
-          <li key={contact.email}>
-            <strong>{contact.name}</strong>: {contact.email}
-          </li>
-        ))}
-      </ul>
+      <div className="input-section">
+        <input
+          className="border-input"
+          type="text"
+          placeholder="Delimiter"
+          value={delimiter}
+          onChange={(e) => setDelimiter(e.target.value)}
+        />
+        <input
+          className="border-input"
+          type="number"
+          placeholder="No. of lines"
+          value={lines}
+          onChange={(e) => setLimitedArray(e)}
+        />
+      </div>
+      {contacts.length > 0 && delimiter === fileDelimiter ? (
+        <table>
+          {filteredContacts.map((contact, index) => (
+            <tr>
+              <td>{contact.Name}</td>
+              <td>{contact.Address} </td>
+              <td>{contact.City} </td>
+              <td>{contact.PinCode}</td>
+            </tr>
+          ))}
+        </table>
+      ) : (
+        <table>
+          {filteredContacts.map((contact, index) => (
+            <tr>
+              <td>
+                {contact.Name}
+                {fileDelimiter}
+                {contact.Address}
+                {fileDelimiter}
+                {contact.City}
+                {fileDelimiter}
+                {contact.PinCode}
+              </td>
+            </tr>
+          ))}
+        </table>
+      )}
     </div>
   );
 }
